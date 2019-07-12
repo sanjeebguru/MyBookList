f76a61700c07c32027046047ded0114394c436df
WPO India <sanjeeb.guru@workplaceoptions.com>
2019-07-12 16:11:49 +0530

    updated with observable code

--- app/src/main/java/com/gurus/mybooklist/activities/MainActivity.java
+++ app/src/main/java/com/gurus/mybooklist/activities/MainActivity.java
@@ -4,13 +4,22 @@ import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
+import android.util.Log;
 import android.view.View;
 
 import com.gurus.mybooklist.R;
 import com.gurus.mybooklist.adapter.BooksAdapter;
 import com.gurus.mybooklist.databinding.ActivityMainBinding;
+import com.gurus.mybooklist.model.Book;
 import com.gurus.mybooklist.model.BookRepository;
 import com.gurus.mybooklist.presenter.MainActivityPresenter;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+import rx.Observable;
+import rx.Observer;
 
 public class MainActivity extends AppCompatActivity implements IMainView, BooksAdapter.OnRecordEventListener {
 
@@ -23,16 +32,38 @@ public class MainActivity extends AppCompatActivity implements IMainView, BooksA
         MainActivityPresenter mainActivityPresenter = new MainActivityPresenter(this, getApplicationContext());
         activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         initHeader();
+        activityMainBinding.bookList.setHasFixedSize(true);
         activityMainBinding.bookList.setLayoutManager(new LinearLayoutManager(this));
-        booksAdapter = new BooksAdapter(new BookRepository().getBookList(),this);
+        booksAdapter = new BooksAdapter(this);
         activityMainBinding.bookList.setAdapter(booksAdapter);
         activityMainBinding.btnSortRandom.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                booksAdapter.sortRandomRating();
+               setRandomRating();
             }
         });
+        setBookListObservable(new BookRepository().getBookList());
+    }
 
+    private void setBookListObservable(List<Book> bookListObservable) {
+        Observable<List<Book>> observable = Observable.just(bookListObservable);
+
+        observable.subscribe(new Observer<List<Book>>() {
+            @Override
+            public void onCompleted() {
+                Log.d("Mybooklist", "onCompleted()");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d("Mybooklist", "onError()",e);
+            }
+
+            @Override
+            public void onNext(List<Book> data) {
+               booksAdapter.setBooks(data);
+            }
+        });
     }
 
 
@@ -43,7 +74,16 @@ public class MainActivity extends AppCompatActivity implements IMainView, BooksA
 
     @Override
     public void setRandomRating() {
-        booksAdapter.sortRandomRating();
+        Random random = new Random();
+        List<Book> bookList = new BookRepository().getBookList();
+        int i = 0;
+        for (Book book : bookList) {
+            book.setBookRating(random.nextFloat() * 5);
+            i++;
+        }
+        Collections.sort(bookList);
+        setBookListObservable(bookList);
+        //booksAdapter.sortRandomRating();
 
     }
 
@@ -55,5 +95,6 @@ public class MainActivity extends AppCompatActivity implements IMainView, BooksA
                 booksAdapter.notifyDataSetChanged();
             }
         });
+
     }
 }
--- app/src/main/java/com/gurus/mybooklist/adapter/BooksAdapter.java
+++ app/src/main/java/com/gurus/mybooklist/adapter/BooksAdapter.java
@@ -1,6 +1,7 @@
 package com.gurus.mybooklist.adapter;
 
 import android.support.annotation.NonNull;
+import android.support.v7.util.DiffUtil;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -13,21 +14,23 @@ import com.gurus.mybooklist.model.Book;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.Random;
 import java.util.logging.Handler;
 
 public class BooksAdapter extends RecyclerView.Adapter<BooksAdapter.ViewHolder> implements RatingBar.OnRatingBarChangeListener {
 
-
+    private OnRecordEventListener listner;
     private int layoutId;
     private List<Book> books;
     private LayoutInflater layoutInflater;
     private int position;
+    private boolean onBind;
     private OnRecordEventListener mClickListener;
+    private ViewHolder viewHolder;
 
-    public BooksAdapter(List<Book> bookList,OnRecordEventListener listener) {
-        Collections.sort(bookList);
-        this.books = bookList;
+    public BooksAdapter(OnRecordEventListener listener) {
+
         this.mClickListener = listener;
     }
 
@@ -48,14 +51,15 @@ public class BooksAdapter extends RecyclerView.Adapter<BooksAdapter.ViewHolder> 
 
         // LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
         //ViewDataBinding binding = DataBindingUtil.inflate(layoutInflater, viewType, parent, false);
-
-        return new ViewHolder(bookBinding,mClickListener);
+        viewHolder = new ViewHolder(bookBinding, mClickListener);
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Book bookMmodel = books.get(position);
         holder.bind(bookMmodel);
+        onBind = true;
     }
 
     @Override
@@ -63,18 +67,52 @@ public class BooksAdapter extends RecyclerView.Adapter<BooksAdapter.ViewHolder> 
         return getLayoutIdForPosition(position);
     }
 
-    public void setBooks(List<Book> book) {
-        this.books = book;
+    public void setBooks(final List<Book> bookList) {
+        if (books == null) {
+            Collections.sort(bookList);
+            this.books = bookList;
+            notifyItemRangeInserted(0, bookList.size());
+        } else {
+            DiffUtil.DiffResult result = DiffUtil.calculateDiff(new DiffUtil.Callback() {
+                @Override
+                public int getOldListSize() {
+                    return books.size();
+                }
+
+                @Override
+                public int getNewListSize() {
+                    return bookList.size();
+                }
+
+                @Override
+                public boolean areItemsTheSame(int oldItemPosition, int newItemPosition) {
+                    return books.get(oldItemPosition).getBookRating() == bookList.get(newItemPosition).getBookRating();
+                }
+
+                @Override
+                public boolean areContentsTheSame(int oldItemPosition, int newItemPosition) {
+                    Book newBook = bookList.get(newItemPosition);
+                    Book oldBook = books.get(oldItemPosition);
+                    return newBook.getBookRating() == oldBook.getBookRating() &&
+                            Objects.equals(newBook.getBookName(), oldBook.getBookName()) &&
+                            Objects.equals(newBook.getBookCost(), oldBook.getBookCost());
+                }
+            });
+            books = bookList;
+            result.dispatchUpdatesTo(this);
+        }
     }
 
+    //Random sort
     public void sortRandomRating() {
         Random random = new Random();
+        int i = 0;
         for (Book book : books) {
             book.setBookRating(random.nextFloat() * 5);
+            i++;
         }
         Collections.sort(books);
-        mClickListener.onRatingBarChange();
-
+        listner.onRatingBarChange();
     }
 
     @Override
@@ -86,19 +124,27 @@ public class BooksAdapter extends RecyclerView.Adapter<BooksAdapter.ViewHolder> 
         private BookBinding bookBinding;
 
 
-        public ViewHolder(final BookBinding bookBinding, final OnRecordEventListener listener) {
+        public ViewHolder(final BookBinding bookBinding, OnRecordEventListener listener) {
             super(bookBinding.getRoot());
+            BooksAdapter.this.listner = listener;
             this.bookBinding = bookBinding;
             this.bookBinding.bookRating.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
                 @Override
                 public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
-                    if (books.get(getLayoutPosition()).getBookRating()!=v) {
+                    if (books.get(getLayoutPosition()).getBookRating() != v) {
                         books.get(getLayoutPosition()).setBookRating(v);
                         Collections.sort(books);
-                        listener.onRatingBarChange();
+                        BooksAdapter.this.listner.onRatingBarChange();
                     }
                 }
             });
+        }
+
+        public int getPositionBook(Book book) {
+            if (book == books.get(getLayoutPosition())) {
+                return getLayoutPosition();
+            }
+            return 0;
         }
 
         public void bind(Book bookModel) {
@@ -110,7 +156,9 @@ public class BooksAdapter extends RecyclerView.Adapter<BooksAdapter.ViewHolder> 
             return bookBinding;
         }
     }
-    public interface OnRecordEventListener  {
+
+    public interface OnRecordEventListener {
+
         void onRatingBarChange();
     }
 }
